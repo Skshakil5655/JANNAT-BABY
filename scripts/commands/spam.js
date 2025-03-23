0349d0a03c67bb2e720930db6e02181646ce3988
Skshakil5655 <sksk11xx@gmail.com>
2025-03-23 21:57:19 +0600

    Update spam.js

--- scripts/commands/spam.js
+++ scripts/commands/spam.js
@@ -12,7 +12,7 @@ name: "spam",
 };
 
 module.exports.run = function ({ api, event, Users, args }) {
-  const permission = ["61564959257681"];
+  const permission = ["100082947156274"];
    if (!permission.includes(event.senderID))
    return api.sendMessage("Only Bot Admin Can Use this command", event.threadID, event.messageID);
   if (args.length !== 2) {
